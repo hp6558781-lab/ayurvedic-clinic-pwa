93a438c80509ea32614265091cc3b05457969e58
hp6558781-lab <hp6558781@gmail.com>
2026-04-08 22:32:28 +0530

    Create welcome page for Ayurvedic Clinic Management System

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WelcomePage = () => {
+    return (
+        <div style={{ textAlign: 'center', margin: '50px' }}>
+            <h1>Welcome to the Ayurvedic Clinic Management System</h1>
+            <p>Your health is our priority. We are here to provide you with the best Ayurvedic treatments.</p>
+        </div>
+    );
+};
+
+export default WelcomePage;
